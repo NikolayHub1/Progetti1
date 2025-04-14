f3bfee1d8befa1eb402430f8b48da74fca2ba18b
NikolayHub1 <Nikynikolov1@gmail.com>
2025-04-14 09:27:21 +0200

    Add files via upload

---
+++ MCD.py
@@ -0,0 +1,21 @@
+print ("benvenuto al McDonegani,ecco il nostro menù")
+scelta=0 
+spesa=0 
+item1=0 
+item2=0
+item3=0 
+print(" i panini sono:")
+panino1=0
+panino2=0
+panino3=0
+if(scelta==1): print("hai scelto il mcChiken,prezzo:5€") 
+spesa=spesa+5;
+item1=scelta; 
+elif(scelta==2):
+print("hai scelto il CrispyMcbacon,prezzo:7€") 
+item1=scelta;
+spesa=spesa+7
+else: 
+print("hai scelto big mc,prezzo:8€") 
+item1=scelta 
+spesa=spesa+8
